f046fbf88792c643edf3ab6e85eaefe4f500fa35
TheRealAxein <112898699+TheRealAxein@users.noreply.github.com>
2022-09-07 20:11:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+from pywebio import start_server
+from pywebio.input import *
+from pywebio.output import *
+from pywebio.session import run_async, run_js
+
+import asyncio
+def aplic():
+    sus = input("Привет")
